0d6f94d0ec6f508af6682cfa547f2a1e3bde5733
Nathaniel Cherian <ngcherian2014@gmail.com>
2024-11-03 11:48:28 -0500

    adding skeleton

---
+++ get.sh
@@ -0,0 +1,13 @@
+# Install containernet
+sudo apt-get install ansible
+git clone https://github.com/containernet/containernet.git
+sudo ansible-playbook -i "localhost," -c local containernet/ansible/install.yml
+
+python -m venv venv
+source venv/bin/activate
+
+cd containernet
+pip install .
+
+cd webserver
+wget https://gist.githubusercontent.com/blakesanie/dde3a2b7e698f52f389532b4b52bc254/raw/76fe1b5e9efcf0d2afdfd78b0bfaa737ad0a67d3/shakespeare.txt
